8f50b750e7115d3a6d1da327a4a84873f35d5351
crelicthecleric <52921332+crelicthecleric@users.noreply.github.com>
2022-03-27 23:51:50 -0400

    Add files via upload

--- groupgraphs.R
+++ groupgraphs.R
@@ -7,6 +7,7 @@ graph_groups <- function(csv, output, title) {
   ggsave(output)
 }
 
-graph_groups("../Desktop/kfold.csv", "../Desktop/kfold.png", "Random Forest Scores vs Fraction of Data with 5-Fold Cross Validation")
-graph_groups("../Desktop/single.csv", "../Desktop/single.png", "Random Forest Scores vs Fraction of Data")
-graph_groups("../Desktop/svm.csv", "../Desktop/svm.png", "SVM Scores vs Fraction of Data")
+graph_groups("./scores/kfold.csv", "./img/kfold.png", "Random Forest Scores vs Fraction of Data with 5-Fold Cross Validation")
+graph_groups("./scores/single.csv", "./img/single.png", "Random Forest Scores vs Fraction of Data")
+graph_groups("./scores/svm.csv", "./img/svm.png", "SVM Scores vs Fraction of Data")
+graph_groups("./scores/zeros.csv", "./img/zeros.png", "Random Forest Scores vs Fraction of 0.5 Data Subset Replaced with Zeros")

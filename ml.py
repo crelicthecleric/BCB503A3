96fdfb758d3058817d0a1f2e7ceb4e023d1a5cd1
crelicthecleric <52921332+crelicthecleric@users.noreply.github.com>
2022-03-28 15:40:05 -0400

    Add files via upload

--- ml.py
+++ ml.py
@@ -1,14 +1,17 @@
 import argparse
+from numpy import mean
+import pandas as pd
 import re
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-import pandas as pd
 
 parser = argparse.ArgumentParser(description='validate classifications')
+parser.add_argument('--kfold', action=argparse.BooleanOptionalAction, help="do k-fold validation")
 parser.add_argument('input', help='input file')
 parser.add_argument('output', help='output file')
 parser.add_argument('model', help='SVM or RF')
@@ -16,13 +19,12 @@ parser.add_argument('treeqty', type=int, nargs='?', default=100, help='Number of
 
 args = parser.parse_args()
 
-def validate_model(input, output, model, numTrees):
-    frac = float(re.findall(r"[0-9][.][0-9]", input)[-1])
+def validate_model(doKfold, input, output, model, numTrees):
+    frac = float(re.findall(r"[0-9][.][0-9]+", input)[-1])
     data = pd.read_csv(input)
     X = data.drop(columns=["activity"])
     X = StandardScaler().fit_transform(X)
     y = data["activity"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
     
     if model == "SVM":
         clf = SVC()
@@ -30,14 +32,19 @@ def validate_model(input, output, model, numTrees):
     elif model == "RF":
         clf = RandomForestClassifier(n_estimators=numTrees, random_state=1)
         name =  "RF: " + str(numTrees) + " Trees"
-    
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    
-    score = f1_score(y_test, y_pred, average='macro')
+        
+    if doKfold:
+        cv = StratifiedKFold(random_state=1, shuffle=True)
+        score = mean(cross_val_score(clf, X, y, scoring='f1_macro', cv=cv, n_jobs=-1))
+    else:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        score = f1_score(y_test, y_pred, average='macro')
+        
     entry = [frac, score, name]
     result = pd.DataFrame([entry], columns=["fraction", "score", "type"])
     result.to_csv(output, index=False)
 
 if __name__ == "__main__":
-    validate_model(args.input, args.output, args.model, args.treeqty)
+    validate_model(args.kfold, args.input, args.output, args.model, args.treeqty)

722d2c5e81518d139d7b01482a6e13a7c9aa5c84
crelicthecleric <52921332+crelicthecleric@users.noreply.github.com>
2022-03-28 16:42:41 -0400

    Add files via upload

--- run_batch.sh
+++ run_batch.sh
@@ -1,8 +1,4 @@
-for frac in `seq 0.2 0.1 0.5`
+for frac in `seq 0.1 0.1 0.9`
 do
-	sbatch run_svm.sh $frac
-	for treeqty in 25 50 100 200 400
-	do
-		sbatch run_rf.sh $frac $treeqty
-	done
+	sbatch run_sub_zeros.sh $frac
 done
